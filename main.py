d41641c4fed4ab1f2595ce43c4221d367459fb27
Vasilis Ethan Sarris <vas82@pitt.edu>
2021-03-01 17:35:33 -0500

    Good Starting Point
    Co-Authored-By: aln96 <55759074+aln96@users.noreply.github.com>
    Co-Authored-By: aaronmathew589 <59574983+aaronmathew589@users.noreply.github.com>
    Co-Authored-By: josephschaub <49382851+josephschaub@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template
-
+from flask_bootstrap import Bootstrap
+from errors import bp as errors_bp
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+app.register_blueprint(errors_bp)
 
 @app.route("/")
 def home():
-    """Return a simple HTML page."""
-    print("Hit the route!")
-    return render_template("index.html")
+    """ Landing Page"""
+    return render_template("home.html")
 
+@app.route("/trails")
+def trails():
+    """ For right now this just loads Andrew's html file """
+    return render_template("fetch_trails_data.html")
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True) 
