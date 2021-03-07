635bc268f12f4975adc9a7a5442aa6955f27a886
aaronmathew589 <aam150@pitt.edu>
2021-03-07 19:50:11 +0000

    Added login.html

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, session
 from flask_bootstrap import Bootstrap
 from errors import bp as errors_bp
 
 app = Flask(__name__)
-
+app.secret_key = "hello"
 bootstrap = Bootstrap(app)
 app.register_blueprint(errors_bp)
 
-@app.route("/")
+@app.route("/home")
 def home():
-    """ Landing Page"""
+    if "user" in session:
+        user = session["user"]
+        return render_template("home.html")
+    else:
+        return redirect(url_for("login"))
     return render_template("home.html")
 
-@app.route("/trails")
+@app.route("/trails")  
 def trails():
     """ For right now this just loads Andrew's html file """
     return render_template("fetch_trails_data.html")
 
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+  form = SearchForm()
+  if not form.validate_on_submit():
+    return redirect(url_for('home'))
+  return redirect((url_for('search_results', query=form.search.data)))
+
+@app.route('/', methods=["POST","GET"])
+def login():
+    session.permanent = False
+    if request.method == "POST":
+        user = request.form['email']
+        password = request.form['password']
+        session["user"] = user
+        return redirect(url_for("home"))
+    else:
+        if "user" in session:
+            return redirect(url_for("home"))
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True) 
